a9a510b5ca4b1589267690b1dffd1e555926da71
Darold So <daroldso@gmail.com>
2015-09-03 01:16:22 +0800

    Prepare dummy data for save to app and load from app methods

--- js/exercise_load.js
+++ js/exercise_load.js
@@ -1,9 +1,12 @@
-var exerciseToLoad = {
-	chapterNum: 1,
-	exerciseNum: 1,
-	isAnswerChecked: true,
-	isAnswerRevealed: false,
-	studentScore: 3,
+var exerciseToLoad = '{"clientref":1,"layout":1,"name":"練習講多謝","instruction":"在以下的情況下，我們要說什麼？","type":"chooseSingleCheckImmediate","questions":[{"index":0,"correct":false,"wrong":false,"isAnswered":false,"questionText":"收到禮物時要講：","answers":[{"correct":false,"selected":false,"text":"我唔鍾意呢個。"},{"correct":true,"selected":false,"text":"多謝你！"}]},{"index":1,"correct":false,"wrong":false,"isAnswered":false,"questionText":"校長讚我表現好時，要講：","answers":[{"correct":true,"selected":false,"text":"多謝！"},{"correct":false,"selected":false,"text":"我鍾意呀！"}]},{"index":2,"correct":false,"wrong":false,"isAnswered":false,"questionText":"人地讚我時，要講：","answers":[{"correct":false,"selected":false,"text":"差不多時候啦！"},{"correct":true,"selected":false,"text":"多謝！"}]},{"index":3,"correct":false,"wrong":false,"isAnswered":false,"questionText":"朋友邀請我參加活動時，要講：","answers":[{"correct":false,"selected":false,"text":"我想去廁所呀。"},{"correct":true,"selected":false,"text":"好呀。多謝你！"}]},{"index":4,"correct":false,"wrong":false,"isAnswered":false,"questionText":"朋友請我吃東西，我要講：","answers":[{"correct":false,"selected":false,"text":"我想食多D！"},{"correct":true,"selected":false,"text":"多謝你，好好味呀！"}]}],"chapterNum":1,"exerciseNum":1,"isAnswerChecked":false,"studentScore":0}';
+
+var exerciseToInit = {
+	chapterNum: 0,
+	exerciseNum: 0,
+	isAnswerChecked: false,
+	isAnswerChecked: false,
+	studentScore: 0,
+	clientref: 1,
 	layout: 1,
 	name: "練習講多謝",
 	instruction: "在以下的情況下，我們要說什麼？",
@@ -17,7 +20,7 @@ var exerciseToLoad = {
 			questionText: "收到禮物時要講：",
 			answers: [
 				{correct: false, selected: false, text: "我唔鍾意呢個。"},
-				{correct: true, selected: true, text: "多謝你！"}
+				{correct: true, selected: false, text: "多謝你！"}
 			]
 		},
 		{
@@ -27,7 +30,7 @@ var exerciseToLoad = {
 			isAnswered: false,
 			questionText: "校長讚我表現好時，要講：",
 			answers: [
-				{correct: true, selected: true, text: "多謝！"},
+				{correct: true, selected: false, text: "多謝！"},
 				{correct: false, selected: false, text: "我鍾意呀！"}
 			]
 		},
@@ -39,7 +42,7 @@ var exerciseToLoad = {
 			questionText: "人地讚我時，要講：",
 			answers: [
 				{correct: false, selected: false, text: "差不多時候啦！"},
-				{correct: true, selected: true, text: "多謝！"}
+				{correct: true, selected: false, text: "多謝！"}
 			]
 		},
 		{
@@ -49,7 +52,7 @@ var exerciseToLoad = {
 			isAnswered: false,
 			questionText: "朋友邀請我參加活動時，要講：",
 			answers: [
-				{correct: false, selected: true, text: "我想去廁所呀。"},
+				{correct: false, selected: false, text: "我想去廁所呀。"},
 				{correct: true, selected: false, text: "好呀。多謝你！"}
 			]
 		},
@@ -60,9 +63,9 @@ var exerciseToLoad = {
 			isAnswered: false,
 			questionText: "朋友請我吃東西，我要講：",
 			answers: [
-				{correct: false, selected: true, text: "我想食多D！"},
+				{correct: false, selected: false, text: "我想食多D！"},
 				{correct: true, selected: false, text: "多謝你，好好味呀！"}
 			]
 		},
 	]
-}
+};
